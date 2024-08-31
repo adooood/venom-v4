178829da3f84166e0aab9cc893c330027c353ea0
adooood <priyanshusharma99553@gmail.com>
2024-08-31 22:17:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7533304441:AAF2HOEddtFKWX_gOgn3zcP2fHCHDq_A5fM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("anshush21887")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5274717770")
 
 
 
